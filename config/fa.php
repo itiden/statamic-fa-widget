9f4681d670faaa283ca9788984de6706f39332ac
neoisrecursive <neo.leijondahl@gmail.com>
2024-01-18 10:42:34 +0100

    Add share url

--- config/fa.php
+++ config/fa.php
@@ -4,4 +4,5 @@ return [
     'api_token' => env('FA_API_TOKEN', null),
     'site_id' => env('FA_SITE_ID', null),
     'hostname' => env('FA_HOSTNAME', null),
+    'share_url' => env('FA_SHARE_URL', null),
 ];
--- resources/views/widgets/fa.blade.php
+++ resources/views/widgets/fa.blade.php
@@ -3,6 +3,6 @@
 </div>
 <div class="text-center text-xs p-4">
     <span>Want more stats? </span>
-    <a href="https://app.usefathom.com/?site={{ config('fa.site_id') }}" target="blank" class="ml-1 underline">Visit
+    <a href="{{ config('fa.share_url') ?? "https://app.usefathom.com/?site=".config('fa.site_id') }}" target="blank" class="ml-1 underline">Visit
         Fathom Analytics</a>
 </div>
